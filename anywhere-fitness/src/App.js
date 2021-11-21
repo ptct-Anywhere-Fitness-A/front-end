ef53c2a72816e2d44ab17f442c8083ad33867ab0
scoutreilly <scouty33@gmail.com>
2021-11-20 20:55:44 -0700

    updating

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,15 +1,12 @@
-import { Route, Switch } from "react-router-dom";
+// import { Route, Switch } from "react-router-dom";
 import "./App.css";
-import Login from "./components/login.js" 
+import Login from "./components/login.js";
 
 function App() {
   return (
     <div className="App">
       <h1>hello welcome to anywhere fitness, where you can fitness anywhere</h1>
-      <Switch>
-        <Route path="/client">{/* include cleint component */}</Route>
-        <Route path="/instructor">{/* include instructor component */}</Route>
-      </Switch>
+      {/* <Switch></Switch> */}
       <Login />
     </div>
   );
