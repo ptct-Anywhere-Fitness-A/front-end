f3817bb62fbd238406518921c51ecb0cb816bbd3
scoutreilly <scouty33@gmail.com>
2021-11-20 20:34:49 -0700

    started on instruuctor

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,9 +1,14 @@
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>hello welcome to anywhere fitness, where you can fitness anywhere</h1>
+      <Switch>
+        <Route path="/client">{/* include cleint component */}</Route>
+        <Route path="/instructor">{/* include instructor component */}</Route>
+      </Switch>
     </div>
   );
 }
---
+++ anywhere-fitness/src/components/Instructor.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Instructor = () => {
+  return <div>{/* display classes object */}</div>;
+};
+
+export default Instructor;
